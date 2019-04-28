af7193588e7678d5a72c26848971030c15557608
Kelly Milligan <kelly@milligan.nz>
2019-04-27 20:08:06 -0400

    Add blank view example

---
+++ _Example.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Example">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Example',
+  props: {},
+  components: {},
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+  .Example {
+
+  }
+
+</style>
